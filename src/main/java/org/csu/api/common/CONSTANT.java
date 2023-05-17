84ee683dade9a12befa1fc2005ccf5d0f728d021
yoyotie <songtie@gmail.com>
2023-05-17 16:04:44 +0800

    完成Category和Produt相关接口

--- src/main/java/org/csu/api/common/CONSTANT.java
+++ src/main/java/org/csu/api/common/CONSTANT.java
@@ -1,8 +1,11 @@
 package org.csu.api.common;
 
+import lombok.Getter;
+
 public class CONSTANT {
 
     public static final String LOGIN_USER = "loginUser";
+    public static final int CATEGORY_ROOT = 0;
 
     public interface ROLE{
         int CUSTOMER = 1;
@@ -14,4 +17,24 @@ public class CONSTANT {
         String EMAIL = "email";
         String PHONE = "phone";
     }
+
+    @Getter
+    public enum ProductStatus{
+
+        ON_SALE(1, "on_sale"),
+        TAKE_DOWN(2, "take_down"),
+        DELETE(3, "delete");
+
+        private final int code;
+        private final String description;
+
+        ProductStatus(int code, String description){
+            this.code = code;
+            this.description = description;
+        }
+    }
+
+    public static final String PRODUCT_ORDER_BY_PRICE_ASC = "price_asc";
+    public static final String PRODUCT_ORDER_BY_PRICE_DESC = "price_desc";
+
 }
--- src/main/java/org/csu/api/controller/front/ProductController.java
+++ src/main/java/org/csu/api/controller/front/ProductController.java
@@ -2,28 +2,40 @@ package org.csu.api.controller.front;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.validation.constraints.NotNull;
+import org.csu.api.domain.Product;
+import org.csu.api.service.ProductService;
+import org.csu.api.vo.ProductDetailVO;
+import org.csu.api.vo.ProductListVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.csu.api.common.CommonResponse;
 
 @RestController
+@RequestMapping("/product/")
+@Validated
 public class ProductController {
-    @GetMapping("detail")
-    public CommonResponse<Object> getProductDetail(
-            @RequestParam @NotNull(message = "商品ID不能为空") Integer productId){
 
-        return null;
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("detail")
+    public CommonResponse<ProductDetailVO> getProductDetail(
+            @RequestParam @NotNull(message = "商品ID不能为空") Integer productId){
+        return productService.getProductDetail(productId);
     }
 
     @GetMapping("list")
-    public CommonResponse<Page<Object>> getProductList(
+    public CommonResponse<Page<ProductListVO>> getProductList(
             @RequestParam(required = false) Integer categoryId,
             @RequestParam(required = false) String keyword,
             @RequestParam(defaultValue = "") String orderBy,
             @RequestParam(defaultValue = "1") int pageNum,
-            @RequestParam(defaultValue = "2") int pageSize){
+            @RequestParam(defaultValue = "5") int pageSize){
 
-        return null;
+        return productService.getProductList(categoryId,keyword,orderBy,pageNum,pageSize);
     }
 }
